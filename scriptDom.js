84e901cf7a34d4c65e68c5130aa48da827f7ed5b
Neha <codewithneha2020@gmail.com>
2021-11-21 19:35:07 +0530

    Delete Feature in Dom

--- scriptDom.js
+++ scriptDom.js
@@ -1,53 +1,76 @@
 const taskContainer = document.querySelector(".task_container");
+let globalStore = [];//array of objects
 console.log(taskContainer);
-
-const globalStore = [];
-
-const generateHtml=(taskData)=>{
-  return `
-  <div id=${taskData.id} class="col-md-6 col-lg-4">
-    <div class="card">
-<div class="card-header d-flex justify-content-end gap-2">
-<button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-<button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
-</div>
-<img src=${taskData.imageurl} class="card-img-top" alt="...">
-<div class="card-body">
-<h5 class="card-title fw-bold text-primary">${taskData.tasktitle}</h5>
-<p class="card-text">${taskData.taskdescription}</p>
-<a href="#" class="btn btn-primary">Go somewhere</a>
-</div>
-</div>
+const generateNewCard = (taskData) => `
+  <div class="col-sm-12 col-md-6 col-lg-4">
+  <div class="card">
+  <div class="card-header d-flex justify-content-end gap-2">
+   <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+  <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"><i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i></button>
   </div>
-  `
+  <div class="card-body">
+  <img class="card-img-top" src=${taskData.imageUrl} alt="...">
+   <h5 class="card-title mt-3 fw-bolder text-primary">${taskData.taskTitle}</h5>
+   <p class="card-text">${taskData.taskDescription}</p>
+   <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+  </div>
+  </div>
+  </div>
+  `;
+
+
+const loadInitialCardData = () => {
+  //localstorage to get tasky card data
+const getCardData = localStorage.getItem("tasky");
+
+
+  //covert to normal object
+const {cards} = JSON.parse(getCardData);
+
+
+  //loop over those array of task obejct to create HTML card , inject it to DOM
+cards.map((cardObject) => {
+  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+
+  //update our globalStore
+  globalStore.push(cardObject);
 }
+
+)
+
+};
+
+
+//Delete function
+
+const deleteCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+
+  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
+  localStorage.setItem("tasky",JSON.stringify({cards: globalStore}));
+
+  if(tagname === "BUTTON") {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  } else {
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }
+};
+
 const saveChanges = () => {
   const taskData = {
     id: `${Date.now()}`,
-    imageurl: document.getElementById("imageURL").value ,
-    tasktitle: document.getElementById("taskTitle").value,
-    tasktype: document.getElementById("taskType").value,
-    taskdescription: document.getElementById("taskDescription").value
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value
   };
-  taskContainer.insertAdjacentHTML("beforeend", newCard);
 
-  globalStore.push(taskData);
 
-  localStorage.setItem("tasky",JSON.stringify({cards: globalStore}));
-}
+taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
 
-const loadInitialData = () =>{
-  const getCardData = localStorage.getItem("tasky"); 
+globalStore.push(taskData);
+localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
 
-  const {cards} = JSON.parse(getCardData);
-
-  cards.map((cardObject)=>{
-    taskContainer.insertAdjacentHTML("beforeend", generateHtml(cardObject))
-    globalStore.push(cardObject)
-  })
-
-}
-//localStorage to get tasky card data 
-  //convert the string to a normal object
-  //loop over the array of task object to create HTML cards , inject it to our dom
-  //update our global store
+};
